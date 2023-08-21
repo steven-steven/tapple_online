1a8d964da1d80173a7f9842ba2bf5318e387ebae
Steven <steven.steven@shopify.com>
2023-08-21 12:19:31 -0400

    Add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import StateProvider from './state-provider'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <StateProvider> { children } </StateProvider>
+        <Analytics mode={'production'} />;
       </body>
     </html>
   )
